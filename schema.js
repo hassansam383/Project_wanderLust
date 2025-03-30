dbf5b09f59cc4b9bb4ccd0652ccba83b6ee4b676
Hasssan <hassansam383@gmail.com>
2025-03-30 12:34:33 +0500

    added review functionality

--- schema.js
+++ schema.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const review = require('./models/review');
 
 module.exports.listingSchema = Joi.object({
     listing : Joi.object({
@@ -11,4 +12,11 @@ module.exports.listingSchema = Joi.object({
 
     }).required()
 });
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required()
+    }).required()
+})
 
